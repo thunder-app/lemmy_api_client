e211fc4e304f7999711d0c0ee753043a5b54eeb1
Zach Russell <zachatrocity@gmail.com>
2023-06-25 15:42:33 -0600

    added back missing string

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -6,5 +6,5 @@ class LemmyApiException implements Exception {
   String get message => _message;
 
   @override
-  String toString() => '$runtimeType: $message';
+  String toString() => '${runtimeType.toString()}: $message';
 }
